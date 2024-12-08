ce2c5c6a3548d0d042674bdfa0c545193729405f
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-12-08 12:42:25 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,9 +8,9 @@
 #   any "hll_rcon_tool" folder on disk.
 # - If your CRCON folder name isn't 'hll_rcon_tool', you must set it here.
 # - Some Ubuntu distros disable 'root' user,
-#   your CRCON could then be found in "/home/ubuntu/hll_rcon_tool".
+#   you may have installed CRCON in "/home/ubuntu/hll_rcon_tool" then.
 # default : "/root/hll_rcon_tool"
-CRCON_folder_path="/root/hll_rcon_tool"
+CRCON_folder_path=""
 
 # Set to "yes" if you have modified any file that comes from CRCON repository
 # First build will take ~3-4 minutes. Subsequent ones will take ~30 seconds.
@@ -24,7 +24,7 @@ rebuild_before_restart="yes"
 # Default : "no"
 redis_cache_flush="no"
 
-# Delete logs before backup
+# Delete logs after backup
 # Default : "no"
 delete_logs="no"
 
@@ -39,8 +39,8 @@ sftp_host=  # no value = disable
 sftp_port=22  # Default : 22
 sftp_dest="/root"
 sftp_user="root"
-delete_after_upload="yes"
-delete_after_upload_dontconfirm="yes"  # Should we always consider the upload successful ?
+delete_after_upload="no"  # set to "yes" if you do not want a local backup
+delete_after_upload_dontconfirm="no"  # Should we always consider the upload successful ?
 
 # Storage informations
 # Default : "no"
@@ -48,55 +48,77 @@ storage_info="yes"
 #
 # └───────────────────────────────────────────────────────────────────────────┘
 
+is_CRCON_configured() {
+  printf "%s└ \033[34m?\033[0m Testing folder : \033[33m%s\033[0m\n" "$2" "$1"
+  if [ -f "$1/compose.yaml" ] && [ -f "$1/.env" ]; then
+    printf "%s  └ \033[32mV\033[0m A valid CRCON install has been found in \033[33m%s\033[0m\n" "$2" "$1"
+  else
+    if [ ! -f "$1/.env" ]; then
+      printf "%s  └ \033[31mX\033[0m Missing file : '\033[37m.env\033[0m'\n" "$2"
+      printf "\n\033[32mWhat to do\033[0m :\nFollow the install procedure to create a '\033[37m.env\033[0m' file\n\n"
+    fi
+    if [ ! -f "$1/compose.yaml" ]; then
+      printf "%s  └ \033[31mX\033[0m Missing file : '\033[37mcompose.yaml\033[0m'\n" "$2"
+      if [ -f "$1/compose.yml" ]; then
+        printf "%s  └ \033[31m!\033[0m Deprecated file found : '\033[37mcompose.yml\033[0m'\n" "$2"
+      fi
+      if [ -f "$1/docker-compose.yml" ]; then
+        printf "%s  └ \033[31m!\033[0m Deprecated file found : '\033[37mdocker-compose.yml\033[0m'\n" "$2"
+      fi
+      printf "\n\033[32mWhat to do\033[0m :\nFollow the install procedure to create a '\033[37mcompose.yaml\033[0m' file\n\n"
+    fi
+    exit
+  fi
+}
+
 clear
 printf "┌─────────────────────────────────────────────────────────────────────────────┐\n"
 printf "│ CRCON restart                                                               │\n"
 printf "└─────────────────────────────────────────────────────────────────────────────┘\n\n"
 
-# User must have root permissions
+this_script_dir=$(dirname -- "$( readlink -f -- "$0"; )";)
 this_script_name=${0##*/}
+
+# User must have root permissions
 if [ "$(id -u)" -ne 0 ]; then
   printf "\033[31mX\033[0m This \033[37m%s\033[0m script must be run with full permissions\n\n" "$this_script_name"
   printf "\033[32mWhat to do\033[0m : you must elevate your permissions using 'sudo' :\n"
   printf "\033[36msudo sh ./%s\033[0m\n\n" "$this_script_name"
   exit
+# Root
 else
   printf "\033[32mV\033[0m You have 'root' permissions.\n"
 fi
 
 # Check CRCON folder path
 if [ -n "$CRCON_folder_path" ]; then
-  crcon_dir=$CRCON_folder_path
   printf "\033[32mV\033[0m CRCON folder path has been set in config : \033[33m%s\033[0m\n" "$CRCON_folder_path"
+  is_CRCON_configured "$CRCON_folder_path" ""
+  crcon_dir="$CRCON_folder_path"
 else
-  printf "\033[34m?\033[0m You didn't set any CRCON folder path in config\n"
-  printf "  Trying to detect a \033[33mhll_rcon_tool\033[0m folder...\n"
-  crcon_dir=$(find / -name "hll_rcon_tool" 2>/dev/null)
-  if [ -n "$crcon_dir" ]; then
-    printf "\033[32mV\033[0m CRCON folder detected in \033[33m%s\033[0m\n" "$crcon_dir"
+  printf "\033[31mX\033[0m You didn't set any CRCON folder path in config\n"
+  printf "└ \033[34m?\033[0m Trying to detect a \033[33mhll_rcon_tool\033[0m folder\n"
+  detected_dir=$(find / -name "hll_rcon_tool" 2>/dev/null)
+  if [ -n "$detected_dir" ]; then
+    is_CRCON_configured "$detected_dir" "  "
+    crcon_dir="$detected_dir"
   else
-    printf "\033[31mX\033[0m No \033[33mhll_rcon_tool\033[0m folder could be found\n\n"
-    printf "  - Maybe you renamed the \033[33mhll_rcon_tool\033[0m folder ?\n"
-    printf "    (it will work the same, but you'll have to adapt every maintenance script)\n\n"
-    printf "  If you followed the official install procedure,\n"
-    printf "  your \033[33mhll_rcon_tool\033[0m folder should be found here :\n"
-    printf "    - \033[33m/root/hll_rcon_tool\033[0m        (most Linux installs)\n"
-    printf "    - \033[33m/home/ubuntu/hll_rcon_tool\033[0m (some Ubuntu installs)\n\n"
-    printf "\033[32mWhat to do\033[0m :Find your CRCON folder, copy this script in it and relaunch it from there.\n\n"
-    exit
+    printf "  └ \033[31mX\033[0m No \033[33mhll_rcon_tool\033[0m folder could be found\n"
+    printf "    └ \033[34m?\033[0m Trying to detect a CRCON install in current folder\n"
+    is_CRCON_configured "$this_script_dir" "      "
+    crcon_dir="$this_script_dir"
   fi
 fi
 
-# Script has to be in the CRCON folder
-this_script_dir=$(dirname -- "$( readlink -f -- "$0"; )";)
+# This script has to be in the CRCON folder
 if [ ! "$this_script_dir" = "$crcon_dir" ]; then
   printf "\033[31mX\033[0m This script is not located in the CRCON folder\n"
   printf "  Script location : \033[33m%s\033[0m\n" "$this_script_dir"
   printf "  Should be here : \033[33m%s\033[0m\n" "$crcon_dir"
-  printf "\033[32mFixing...\033[0m\n"
+  printf "  \033[32mTrying to fix...\033[0m\n"
   cp "$this_script_dir/$this_script_name" "$crcon_dir"
   if [ -f "$crcon_dir/$this_script_name" ]; then
-    printf "\033[32mV\033[0m \033[37m%s\033[0m has been copied in \033[33m%s\033[0m\n\n" "$this_script_name" "$crcon_dir"
+    printf "  \033[32mV\033[0m \033[37m%s\033[0m has been copied in \033[33m%s\033[0m\n\n" "$this_script_name" "$crcon_dir"
     printf "\033[32mWhat to do\033[0m : enter the CRCON folder and relaunch the script using this command :\n"
     printf "\033[36mrm %s && cd %s && sudo sh ./%s\033[0m\n\n" "$this_script_dir/$this_script_name" "$crcon_dir" "$this_script_name"
     exit
@@ -112,7 +134,7 @@ fi
 # Script has to be launched from CRCON folder
 current_dir=$(pwd | tr -d '\n')
 if [ ! "$current_dir" = "$crcon_dir" ]; then
-  printf "\033[31mX\033[0m This \033[37m%s\033[0m script should be run from the CRCON folder\n\n" "$this_script_name"
+  printf "\033[31mX\033[0m This script should be run from the CRCON folder\n\n"
   printf "\033[32mWhat to do\033[0m : enter the CRCON folder and relaunch the script using this command :\n"
   printf "\033[36mcd %s && sudo sh ./%s\033[0m\n\n" "$crcon_dir" "$this_script_name"
   exit
@@ -120,31 +142,7 @@ else
   printf "\033[32mV\033[0m This script has been run from the CRCON folder\n"
 fi
 
-# CRCON config check
-if [ ! -f "$crcon_dir/compose.yaml" ] || [ ! -f "$crcon_dir/.env" ]; then
-  printf "\033[31mX\033[0m CRCON doesn't seem to be configured\n"
-  if [ ! -f "$crcon_dir/compose.yaml" ]; then
-    printf "  \033[31mX\033[0m There is no '\033[37mcompose.yaml\033[0m' file in \033[33m%s\033[0m\n" "$crcon_dir"
-  fi
-  if [ ! -f "$crcon_dir/.env" ]; then
-    printf "  \033[31mX\033[0m There is no '\033[37m.env\033[0m' file in \033[33m%s\033[0m\n" "$crcon_dir"
-  fi
-  printf "\n\033[32mWhat to do\033[0m : check your CRCON install in \033[33m%s\033[0m\n\n" "$crcon_dir"
-  exit
-else
-  printf "\033[32mV\033[0m CRCON seems to be configured\n"
-fi
-
 printf "\033[32mV Everything's fine\033[0m Let's backup this CRCON !\n\n"
-
-if [ $rebuild_before_restart = "yes" ]; then
-  echo "┌──────────────────────────────────────┐"
-  echo "│ Build CRCON                          │"
-  echo "└──────────────────────────────────────┘"
-  docker compose build
-  echo "└──────────────────────────────────────┘"
-  printf "Build CRCON : \033[32mdone\033[0m.\n\n"
-fi
 
 echo "┌──────────────────────────────────────┐"
 echo "│ Stop CRCON                           │"
@@ -187,6 +185,15 @@ if [ $delete_logs = "yes" ]; then
   printf "Delete logs : \033[32mdone\033[0m.\n\n"
 fi
 
+if [ $rebuild_before_restart = "yes" ]; then
+  echo "┌──────────────────────────────────────┐"
+  echo "│ Build CRCON                          │"
+  echo "└──────────────────────────────────────┘"
+  docker compose build
+  echo "└──────────────────────────────────────┘"
+  printf "Build CRCON : \033[32mdone\033[0m.\n\n"
+fi
+
 echo "┌──────────────────────────────────────┐"
 echo "│ Restart CRCON                        │"
 echo "└──────────────────────────────────────┘"
@@ -206,7 +213,6 @@ if [ $clean_docker_stuff = "yes" ]; then
   printf "Clean Docker stuff : \033[32mdone\033[0m.\n\n"
 fi
 
-# SFTP transfer is configured
 if [ -n "$sftp_host" ]; then
   echo "┌──────────────────────────────────────┐"
   echo "│ Uploading backup to another machine  │"
@@ -255,7 +261,8 @@ if [ $storage_info = "yes" ]; then
   { printf "CRCON total size     : "; du -sh "$crcon_dir" | tr -d '\n'; }
   printf "\n────────────────────────────────────────"
   { printf "\n └ Database          : "; du -sh "$crcon_dir"/db_data | tr -d '\n'; }
-  db_command="docker exec -it hll_rcon_tool-postgres-1 psql -U rcon -d rcon -t -A -c "
+  # db_command="docker exec -it hll_rcon_tool-postgres-1 psql -U rcon -d rcon -t -A -c "
+  db_command="docker compose exec -it postgres psql -U rcon -d rcon -t -A -c "
   db_table_size="SELECT pg_size_pretty(pg_total_relation_size('public."
   db_rows_count="SELECT COUNT(*) FROM public."
   { printf "\n   └ audit_log       : "; ($db_command "$db_table_size""audit_log'));") | tr -d ' \t\r\n'; printf "\t("; ($db_command "$db_rows_count""audit_log";) | tr -d ' \t\r\n'; printf " rows)\n"; }
@@ -275,7 +282,7 @@ printf "┌───────────────────────
 printf "│ \033[32mBackup done\033[0m                          │\n"
 printf "└──────────────────────────────────────┘\n"
 if [ -n "$sftp_host" ]; then
-  printf "Compressed backup file has been uploaded on\n"
+  printf "Your compressed backup file has been uploaded on\n"
   printf "\033[33m%s\033[0m, as \033[33m%s/\033[0m%s\n" "$sftp_host" "$sftp_dest" "$backup_name"
   printf "This file size is %s\n\n" "$backup_file_size"
   if [ "$local_deleted" = "yes" ]; then
